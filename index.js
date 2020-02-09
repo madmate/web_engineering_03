863f6ca3b4c1df1937be28c041067bffc7892d8a
Markus Neugebauer <mail@markusneugebauer.de>
2020-02-09 20:40:29 +0100

    bot setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply(`Welcome ${ctx.from.first_name}`));
+bot.launch();
